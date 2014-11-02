e019c8ac84718430c701c33835921baff8aea6bd
ABHIMUCH <abhi.much@gmail.com>
2014-11-02 11:43:33 -0500

    basic reading from .csv and then creating twilio message

---
+++ CSV/src/csvreader/Reader.java
@@ -0,0 +1,48 @@
+package csvreader;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import com.twilio.sdk.TwilioRestException;
+
+
+public class Reader {
+
+	 public static void main(String[] args) throws FileNotFoundException, NumberFormatException, TwilioRestException 
+	    {
+		 
+		 	//Create a Twilio Object
+		 	Twilio trial = new Twilio();
+		 
+	        //Get scanner instance
+	        Scanner scanner = new Scanner(new File("//Users//abhimanyumuchhal//Desktop//Mich-Freshman//Research//data.csv"));
+	         
+	        //Set the delimiter used in file
+	        scanner.useDelimiter(",");
+	         
+	        //Get all tokens and store them in some data structure
+	        //I am just printing them
+	        while (scanner.hasNext()) 
+	        {
+	        	scanner.nextLine();
+	        	
+	        	for(int i = 1; i < 6; i++)
+	        		scanner.next();
+	     
+	        	String s = scanner.next();
+	        	if(!s.equals("")){
+	        		System.out.println(s);
+	        		trial.sendMessage(Integer.parseInt(s));
+	        		
+	        		
+	        	}
+	            scanner.nextLine();
+	            
+	        }
+	         
+	        //Do not forget to close the scanner  
+	        scanner.close();
+	    }
+
+}
---
+++ CSV/src/csvreader/Twilio.java
@@ -0,0 +1,55 @@
+package csvreader;
+
+import com.twilio.sdk.TwilioRestClient;
+import com.twilio.sdk.TwilioRestException;
+import com.twilio.sdk.resource.factory.SmsFactory;
+import com.twilio.sdk.resource.instance.Sms;
+import com.twilio.sdk.resource.list.SmsList;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+public class Twilio {
+
+	  // Find your Account Sid and Token at twilio.com/user/account
+	 private static final String ACCOUNT_SID = "AC781d1a15b57b265850465913c830bfa5"; 
+	 private static final String AUTH_TOKEN = "6060950865885af435f07f6d8e451e86"; 
+
+	  
+	  public void sendMessage(int val) throws TwilioRestException{
+		  
+		  String toSend = message(val);
+		  TwilioRestClient client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN);
+			 
+		  if(!toSend.equals("")){
+		  
+		    // Build a filter for the SmsList
+		    List<NameValuePair> params = new ArrayList<NameValuePair>();
+		    params.add(new BasicNameValuePair("Body", "Abhi says Your Blood Glucose Level: " + val +  toSend));
+		    params.add(new BasicNameValuePair("From", "+16467628959")); 
+		    params.add(new BasicNameValuePair("To", "+19177506286"));
+		     
+		     
+		    SmsFactory smsFactory = client.getAccount().getSmsFactory();
+		    Sms sms = smsFactory.create(params);
+		    System.out.println(sms.getSid());
+		    
+		  }
+		    
+	  }
+	  
+	  private String message(int val){
+		  
+		  if( val < 120)
+			  return " is too high";
+		  
+		  else if(val > 240)
+			  return " is too low";
+		  
+		  else
+			  return "";
+	  }
+	  
+	
+}
