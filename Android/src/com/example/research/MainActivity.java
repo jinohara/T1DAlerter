d16860256d61dd1a07c8f76ccee47843813bbfa5
ABHIMUCH <abhi.much@gmail.com>
2015-02-05 23:04:55 -0500

    Update MainActivity.java

--- Android/src/com/example/research/MainActivity.java
+++ Android/src/com/example/research/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.research;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -15,7 +19,14 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         
         
-    
+        Intent i = new Intent(getApplicationContext(), DatabaseActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(getApplicationContext(),3333,i,
+        PendingIntent.FLAG_CANCEL_CURRENT);
+        AlarmManager mgr = (AlarmManager)this.getSystemService(Context.ALARM_SERVICE);
+        mgr.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() , 300000, pi);
+
+
+        
         Button button = (Button) findViewById(R.id.button1);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
