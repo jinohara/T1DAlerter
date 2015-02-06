255aaf7ae00eaf85c31ed9bb34d29afec230483f
ABHIMUCH <abhi.much@gmail.com>
2015-02-05 22:10:49 -0500

    Database Changed

--- Android/src/com/example/research/DatabaseActivity.java
+++ Android/src/com/example/research/DatabaseActivity.java
@@ -5,6 +5,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
 
@@ -14,76 +15,46 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class DatabaseActivity extends Activity {
+public class DatabaseActivity extends Activity implements MyResultReceiver.Receiver  {
 
-	private final String databaseName = "jcostik-nightscout";
-	private final String collectionName = "entries";
-	private final String apiKey = "CR4PAAj5PmApVtW6XKHTGp8sMkmug76a";
-	private final static int COUNT = 2;
-	private ArrayList<String> result;
+	public MyResultReceiver mReceiver;
 
-	private class MongoLab extends
-			AsyncTask<String, ArrayList<String>, ArrayList<String>> {
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+		mReceiver = new MyResultReceiver(new Handler());
+		mReceiver.setReceiver(mReceiver);
+		final Intent intent = new Intent(Intent.ACTION_SYNC, null, this,
+				QueryService.class);
+		intent.putExtra("receiver", mReceiver);
+		intent.putExtra("command", "query");
+		startService(intent);
+	}
 
-		protected ArrayList<String> doInBackground(String... params) {
+	public void onPause() {
+		mReceiver.setReceiver(null); // clear receiver so no leaks.
+	}
 
-			
-			String urlString = params[0];
-			String line;
-
-			try {
-				HttpURLConnection urlConnection = (HttpURLConnection) new URL(
-						urlString).openConnection();
-				urlConnection.connect();
-				Scanner database = new Scanner(new InputStreamReader(
-						urlConnection.getInputStream()));
-				database.useDelimiter("\"_id\"");
-				line = database.next();
-
-				for (int i = 0; i < COUNT && database.hasNext(); i++) {
-					line = database.next();
-					result.add(line);
-					Log.d("DatabaseQuery", line);
-
-				}
-	
-				 Intent myIntent = new Intent(DatabaseActivity.this,
-							ResultActivity.class);
-							 myIntent.putExtra("val", result);
-							 DatabaseActivity.this.startActivity(myIntent);
-
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
-				return null;
-			}
-
-			return null;
+	public void onReceiveResult(int resultCode, Bundle resultData) {
+		switch (resultCode) {
+		case 0:
+			// RUNNING
+			break;
+		case 1:
+			List results = resultData.getParcelable("results");
+			// do something interesting
+			// hide progress
+			// FINISHED
+			break;
+		case 2:
+			// handle the error;
+			break;
 		}
 	}
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_database);
-		result = new ArrayList<String>();
-		String url = "https://api.mongolab.com/api/1/databases/" + databaseName
-				+ "/collections/" + collectionName + "?apiKey=" + apiKey + "";
-		new MongoLab().execute(url);
-		
-
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.database, menu);
-		return true;
-	}
-
 }
