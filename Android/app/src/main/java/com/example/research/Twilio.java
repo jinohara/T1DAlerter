323ec273fe2712ecb34ef3ef0cb3891b1efab433
ABHIMUCH <abhi.much@gmail.com>
2015-03-27 12:23:30 -0400

    added reading from file code

--- Android/app/src/main/java/com/example/research/Twilio.java
+++ Android/app/src/main/java/com/example/research/Twilio.java
@@ -17,24 +17,24 @@ public class Twilio {
     private static final String AUTH_TOKEN = "6060950865885af435f07f6d8e451e86";
 
 
-    public Twilio (){
+    public static void sendMessage(int val) throws TwilioRestException{
 
-    }
-    public void sendMessage(int val) throws TwilioRestException{
-
-        //add stuff here
         String toSend = "";
         TwilioRestClient client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN);
+
+        if(val == 1)
+            toSend = " IS TOO HIGH";
+        else if (val == -1)
+            toSend = " IS TOO LOW";
 
         if(!toSend.equals("")){
 
             // Build a filter for the SmsList
             List<NameValuePair> params = new ArrayList<NameValuePair>();
-            params.add(new BasicNameValuePair("Body", "Abhi says Your Blood Glucose Level: " + val +
-                    "," + toSend));
+            params.add(new BasicNameValuePair("Body", "Abhi says Your Blood Glucose Level" +
+                     toSend));
             params.add(new BasicNameValuePair("From", "+16467628959"));
             params.add(new BasicNameValuePair("To", "+19177506286"));
-
 
             SmsFactory smsFactory = client.getAccount().getSmsFactory();
             Sms sms = smsFactory.create(params);
